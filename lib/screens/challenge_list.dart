f003a6f2e4fd8fd1c49d53e739307237cde24cb6
jjy0921 <jjy092191@gmail.com>
2023-07-27 20:09:08 +0900

    [FEAT] challenge

--- lib/screens/challenge_list.dart
+++ lib/screens/challenge_list.dart
@@ -1,20 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:miri_app/screens/challeng.dart';
 
 // class StartScreen extends StatefulWidget {
 //   @override
 //   _StartScreenState createState() => _StartScreenState();
 // }
 
-class ChallengeListScreen extends StatelessWidget {
+void main() {
+  runApp(const MaterialApp(
+    home: ChallengeListScreen(),
+  ));
+}
+
+class ChallengeListScreen extends StatefulWidget {
+  const ChallengeListScreen({super.key});
+
+  @override
+  ChallengeListScreenState createState() => ChallengeListScreenState();
+}
+
+class ChallengeListScreenState extends State<ChallengeListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("챌린지"),
+        // title: Text('My App'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
-      body: Container(
-        child: Center(
-          child: Text("Challenge List Screen"),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text(
+                '챌린지',
+                style: TextStyle(fontSize: 24),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                '14일동안 미션을 수행해보아요',
+                style: TextStyle(fontSize: 14),
+              ),
+              const SizedBox(height: 20),
+              ...List.generate(14, (index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 100,
+                    width: 1000,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ChallengPage(day: index + 1),
+                          ),
+                        );
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            const Color(0xff6B42F8)),
+                      ),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Day ${index + 1}',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }),
+            ],
+          ),
         ),
       ),
     );
