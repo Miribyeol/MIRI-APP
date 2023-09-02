0ec45da4370b81e0639953d971b60a6bdb436621
Hyu-noo <lehw20@naver.com>
2023-09-02 22:04:33 +0900

    로그아웃 수정

--- lib/screens/mypage.dart
+++ lib/screens/mypage.dart
@@ -1,11 +1,9 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart'; // 추가
+//import 'start.dart';
 
 class MypageScreen extends StatelessWidget {
-  final FlutterSecureStorage _storage = const FlutterSecureStorage();
-
-  const MypageScreen({super.key}); // 추가
+  final FlutterSecureStorage _storage = const FlutterSecureStorage(); // 추가
 
   Future<void> _logout(context) async {
     await _storage.delete(key: 'jwt_token'); // jwt_token 토큰값 삭제
@@ -14,6 +12,10 @@ class MypageScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double buttonHeight = screenHeight * 0.13;
+    double buttonTitleSize = 20;
+    
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xFF6B42F8),
@@ -35,48 +37,48 @@ class MypageScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const SizedBox(height: 40),
+            SizedBox(height: screenHeight *0.04),
             ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/mypage_my_info');
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color(0xFF1F2839),
-                minimumSize: const Size(double.infinity, 110),
+                minimumSize: Size(double.infinity, buttonHeight),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10.0),
                 ),
                 padding: EdgeInsets.zero, // 버튼 내부 패딩 제거
               ),
-              child: const Padding(
-                padding: EdgeInsets.fromLTRB(16.0, 0, 16.0, 0), // 왼쪽 패딩 추가
+              child: Padding( //const 없앰
+                padding: const  EdgeInsets.fromLTRB(16.0, 0, 16.0, 0), // 왼쪽 패딩 추가
                 child: Align(
                   alignment: Alignment.centerLeft, // 텍스트를 왼쪽으로 정렬
                   child: Text(
                     '내 정보 관리',
                     style: TextStyle(
                       color: Colors.white,
-                      fontSize: 20.0,
+                      fontSize: buttonTitleSize,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
               ),
             ),
-            const SizedBox(height: 30),
+            SizedBox(height: screenHeight * 0.03),
             ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/mypage_pet_info');
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color(0xFF1F2839),
-                minimumSize: const Size(double.infinity, 110),
+                minimumSize:  Size(double.infinity, buttonHeight),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10.0),
                 ),
                 padding: EdgeInsets.zero, // 버튼 내부 패딩 제거
               ),
-              child: const Padding(
+              child: Padding( //const 없앰
                 padding: EdgeInsets.fromLTRB(16.0, 0, 16.0, 0), // 왼쪽 패딩 추가
                 child: Align(
                   alignment: Alignment.centerLeft, // 텍스트를 왼쪽으로 정렬
@@ -84,24 +86,25 @@ class MypageScreen extends StatelessWidget {
                     '반려동물 정보 관리',
                     style: TextStyle(
                       color: Colors.white,
-                      fontSize: 20.0,
+                      fontSize: buttonTitleSize,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
               ),
             ),
-            const SizedBox(height: 380),//430
+            SizedBox(height: screenHeight* 0.43 ),//430 //400
             ElevatedButton(
               onPressed: () {
                 _logout(context); // 로그아웃 버튼을 눌렀을 때 로그아웃 처리
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color(0xff3a3b50),
-                minimumSize: const Size(double.infinity, 50),
+                minimumSize: Size(double.infinity, buttonHeight*0.5),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10.0),
                 ),
+                 padding: const EdgeInsets.symmetric(horizontal: 10.0),
               ),
               child: const Align(
                 alignment: Alignment.center,
@@ -115,7 +118,7 @@ class MypageScreen extends StatelessWidget {
                 ),
               ),
             ),
-       
+        
         ],
         ),
       ),
